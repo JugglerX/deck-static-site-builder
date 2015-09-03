de2330891d3a3818aeedac8090bcaaabac04d95f
JugglerX <rob@jugglerdigital.com>
2015-09-03 10:57:38 +1000

    add select boxes for html, css and json. add basic styling

--- app.js
+++ app.js
@@ -1,67 +1,89 @@
+// Define available Library items. Eventually this will be created based on scanning the /library/ folder structure (using node FS)
+var library = [
+  ["airbnb","https://zilyo.p.mashape.com/id?id=air3962084"],
+  ["issue","http://localhost:3000/api/issues/1"]
+];
+
+var globalJson = {};
+var globalHtml = {};
+
+// Boot the App
 jQuery(document).ready(function($) {
-  $(".colorblue").click(function(){
-    getCSS('blue')
-    return false;
-  })
 
-  getTemplate("hotel");
-  getCSS("hotel");
-  getJson();
+  // Populate the select boxes from the library array
+  populateTemplateSelectBox(library)
+  populateHtmlSelectBox(library)
+  populateCssSelectBox(library);
+  populateJsonSelectBox(library);
 
-  $( ".template-selector" ).change(function() {
-    var str = "";
-    $( "select option:selected" ).each(function() {
-      str = $(this).text();
-    });
-    console.log(str)
-    loadTemplate(str);
-  });
+  // Load a default template
+  loadTemplate("airbnb","https://zilyo.p.mashape.com/id?id=air3962084")
+
+  // Hook on change events
+  changeTemplate();
+  changeCss();
+  changeJson();
+  changeHtml();
+
 });
 
-Handlebars.registerHelper('helperMissing', function(/* [args, ] options */) {
-    var options = arguments[arguments.length - 1];
-    return 'MISSING VALUE: ' + options.name;
-});    
 
-function getTemplate(path) {
+Handlebars.registerHelper("getStudentValue", function(val) {
+    if(val === undefined) {
+        return "null";
+    }
+    return val;
+});
+
+
+function loadTemplate(template, url) {
+  console.log("Load Template")
+  getJson(url);
+  getHtml(template);
+  getCss(template); 
+}
+
+function getHtml(path) {
   var asset = "library/" + path + "/" + path + ".html"
-  console.log(asset);
-
+  console.log("HTML Asset: " + asset);
   $.ajax({
     url: asset,
     cache: true,
     success: function(data) {
-    	console.log(data)
       source = data;
       template = Handlebars.compile(source);
+      updateHtmlCodeHighlight(asset);
+      console.log(globalJson)
+      $(".deck").html(template(globalJson));   
+    },
+    error: function(err) { 
+      console.log(path)
     }
   });
 };
 
-function getCSS(path) {
+function getCss(path) {
   var asset = "library/" + path + "/" + path + ".css"
-  console.log(asset);
-
+  console.log("CSS Asset: " + asset);
   $("#template-css").attr("href", asset);
+
   updateCssCodeHighlight(asset);
 };
 
-function getJson() {
+function getJson(path) {
+  console.log("JSON Asset: " + path);
   var request = $.ajax({
-    url: "https://zilyo.p.mashape.com/id?id=air3962084",
+    url: path,
     type: 'GET', 
-    data: {}, // Additional parameters here
+    data: {}, 
     dataType: 'json',
     success: function(data) { 
-      console.log(data);
+      globalJson = data
       updateJsonCodeHighlight(data);
-      $(".deck").html(template(data));   
-      for (i in data) {
-        console.log(i)
-      }
+      $(".deck").html(template(data));  
     },
     error: function(err) { 
-      alert(err); 
+      console.log(path)
     },
     beforeSend: function(xhr) {
       xhr.setRequestHeader("X-Mashape-Authorization", "0c0ftQWSlxmshp0yAhOSa50Pg7zMp1BBxEDjsnQdZKJcvcX9M0"); 
@@ -69,6 +91,89 @@ function getJson() {
   })
 };
 
+
+
+function changeTemplate() {
+  $( "#template-selector" ).change(function() {
+    var str = [];
+    console.log("**** Change Template ****")
+    $(this).children("select option:selected").each(function() {
+      str = $(this).text().split(',');
+      console.log(str)
+    });
+    loadTemplate(str[0], str[1]);
+  });
+};
+
+function changeCss() {
+  $( "#css-selector" ).change(function() {
+    var str = '';
+    $(this).children("select option:selected").each(function() {
+      str = $(this).text();
+      console.log(str)
+    });
+    getCss(str);
+  });
+};
+
+function changeJson() {
+  $( "#json-selector" ).change(function() {
+    var str = '';
+    $(this).children("select option:selected").each(function() {
+      str = $(this).text();
+      console.log(str)
+    })
+    getJson(str);
+  });
+};
+
+function changeHtml() {
+  $( "#html-selector" ).change(function() {
+    var str = '';
+    
+    $(this).children("select option:selected").each(function() {
+      str = $(this).text();
+      console.log(str)
+    });
+    
+    getHtml(str);
+  });
+};
+
+// Populate Select Boxes
+function populateTemplateSelectBox(array) {
+  var option = '';
+  for (var i=0;i<array.length;i++){
+     option += '<option value="'+ array[i] + '">' + array[i] + '</option>';
+  }
+  $('#template-selector').append(option);
+}
+
+function populateHtmlSelectBox(array) {
+  option = '';
+  for (var i=0;i<array.length;i++){
+     option += '<option value="'+ array[i][0] + '">' + array[i][0] + '</option>';
+  }
+  $('#html-selector').append(option);
+}
+
+function populateCssSelectBox(array) {
+  option = '';
+  for (var i=0;i<array.length;i++){
+     option += '<option value="'+ array[i][0] + '">' + array[i][0] + '</option>';
+  }
+  $('#css-selector').append(option);
+}
+
+function populateJsonSelectBox(array) {
+  option = '';
+  for (var i=0;i<array.length;i++){
+     option += '<option value="'+ array[i][1] + '">' + array[i][1] + '</option>';
+  }
+  $('#json-selector').append(option);
+}
+
+// Update code highlight boxes on the DOM
 function updateJsonCodeHighlight(data) {
   $(".json code").html(JSON.stringify(data, null, 2));
   Prism.highlightElement($('.json code')[0]);
@@ -79,9 +184,8 @@ function updateCssCodeHighlight(data) {
   Prism.fileHighlight();
 }
 
-function loadTemplate(template) {
-  getTemplate(template);
-  getCSS(template);
-  getJson()
+function updateHtmlCodeHighlight(data) {
+  $(".html pre").attr('data-src', data);
+  Prism.fileHighlight();
 }
 
