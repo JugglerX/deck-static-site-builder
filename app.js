2134449ba70371bd18272b4d4fe96d64b675c826
JugglerX <rob@jugglerdigital.com>
2015-09-04 15:56:58 -0700

    style hotel card

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function($) {
   populateJsonSelectBox(library);
 
   // Load a default template
-  loadTemplate("airbnb","https://zilyo.p.mashape.com/id?id=air3962084")
+  loadTemplate("hotel","https://zilyo.p.mashape.com/id?id=air3962084")
 
   // Hook on change events
   changeTemplate();
@@ -28,59 +28,21 @@ jQuery(document).ready(function($) {
 });
 
 
-Handlebars.registerHelper('helperMissing', function(/* [args, ] options */) {
-    // if(arguments === undefined || arguments === "" || arguments === null) {
-    //     return "null";
-    // }
-    console.log(arguments)
-    for (var key in arguments) {
-      console.log(arguments[key])
-      console.log(key)
-        console.log('first level')
-        console.log('')
-      for (var i in arguments[key]) {
-        console.log(arguments[key][i])
-        console.log(i)
-        console.log('second level')
-        console.log('')
-        for (var x in arguments[key][i]) {
-          console.log(arguments[key][i][x])
-          console.log(x)
-                  console.log('third level')
-          console.log('')
-        }
-      }
+// Handlebars.registerHelper('helperMissing', function(/* [args, ] options */) {
+//     var options = arguments[arguments.length - 1];
+//     return 'MISSING VALUE: ' + options.name;
+// });  
+
+Handlebars.registerHelper('blockHelperMissing', function(context, options) {
+  throw new Handlebars.Exception('Only if or each is allowed');
+});
+
+Handlebars.registerHelper("nullValue", function(val) {
+    if(val === undefined) {
+        return "{{" + val + "}}";
     }
-    // if(arguments === Object(arguments)) {
-    //   for (var key in arguments) {
-    //     if (arguments.hasOwnProperty(key)) {
-    //       if (arguments[key] === undefined) {
-    //         return "null"
-    //       }
-    //       for (var i in arguments[key]) {
-    //         if (arguments[key].hasOwnProperty(i)) {
-    //             if (arguments[key][i] === undefined) {
-    //               return "null"
-    //             }
-    //         }
-    //       }
-    //       // console.log("fuck yea")
-    //       // console.log(arguments[key])
-    //       // console.log(key)
-    //     }
-    //     // console.log(arguments)
-    //   }
-    // } 
-    return arguments;
-}); 
-
-// Handlebars.registerHelper("getStudentValue", function(val) {
-//     if(val === undefined) {
-//         return "null";
-//     }
-//     return val;
-// });
-
+    return val;
+});
 
 function loadTemplate(template, url) {
   console.log("Load Template")
@@ -97,7 +59,8 @@ function getHtml(path) {
     cache: true,
     success: function(data) {
       source = data;
-      template = Handlebars.compile(source);
+      template = Handlebars.compile(source,{compat: true});
+      
       updateHtmlCodeHighlight(asset);
       console.log(globalJson)
       $(".deck").html(template(globalJson)); 
@@ -137,8 +100,6 @@ function getJson(path) {
     }
   })
 };
-
-
 
 function changeTemplate() {
   $( "#template-selector" ).change(function() {
