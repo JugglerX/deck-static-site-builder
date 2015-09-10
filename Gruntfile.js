3756b61aac956fce29edbda4615d96f217b3051b
JugglerX <rob@jugglerdigital.com>
2015-09-10 12:05:38 -0700

    add gruntfile, sass compiler and livingstyleguide watchers

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,42 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      sass: {
+        files: ['framework/deck/scss/**/*.scss'],
+        tasks: ['sass']
+      },
+      livingstyleguide: {
+        files: ['framework/deck/scss/**/*.md'],
+        tasks: ['livingstyleguide']
+      },
+    },
+    sass: {
+      options: {
+          sourceMap: false,
+          livereload: true
+      },
+      dist: {
+          files: {
+              'framework/deck/scss/style.css': 'framework/deck/scss/style.scss'
+          }
+      }
+    },
     livingstyleguide: {
       generate: {
         options: {
-          src: 'css/scss/styleguide.lsg',
+          src: 'framework/deck/scss/styleguide.lsg',
           dest: 'styleguide.html'
         }
       }
-    }
+    },
+
   });
 
   grunt.loadNpmTasks('grunt-livingstyleguide');
-  
-  grunt.registerTask('default', ['livingstyleguide']);
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
 
+  grunt.registerTask('default', ['sass','livingstyleguide']);
 
 };
--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var livingstyleguide = require('gulp-livingstyleguide');
-
-gulp.task('default', function () {
-    gulp.src('css/scss/styleguide.lsg')
-        .pipe(livingstyleguide())
-        .pipe(gulp.dest('public'));
-});
